1f5f5acff16832efd04cfeb518a902a654f0ab6f
Yun Zhi Lin <yunzhi246@gmail.com>
2020-03-21 00:38:57 -0400

    Linked tile and determining winning hand

--- determine_winning_hand.py
+++ determine_winning_hand.py
@@ -1,24 +1,33 @@
 import copy
 import pprint
+from tile import Tile
+
+
+JINGTING_RULES = True
 
 
 class Hand:
-    def __init__(self, pair, sets, left):
+    def __init__(self, pair, triples, sequences, left):
         self.pair = pair
-        self.sets = sets
+        self.triples = triples
+        self.sequences = sequences
         self.left = left
+        self.name = "Pair: "+str(self.pair) + "  Triples: "+str(self.triples) + "  Sequences: "+str(self.sequences)
+        if len(self.left) > 0:
+            self.name = self.name + "  Left: "+str(self.left)
 
     def __repr__(self):
-        str_rep = "Pair: " + str(self.pair) + "  Sets: " + str(self.sets)
-        if len(self.left) > 0:
-            str_rep = str_rep + "  Left: " + str(self.left)
-        return str_rep
+        return self.name
 
     def __str__(self):
-        str_rep = "Pair: " + str(self.pair) + "  Sets: " + str(self.sets)
-        if len(self.left) > 0:
-            str_rep = str_rep + "  Left: " + str(self.left)
-        return str_rep
+        return self.name
+
+
+def check_terminals_winds_dragons(num_list):
+    for n in num_list:
+        if Tile.is_terminal_wind_dragon_from_comparison_value(n):
+            return True
+    return False
 
 
 def get_pairs(nums_list):
@@ -39,7 +48,7 @@ def get_triples(nums_list):
 
 def get_sequences(nums_list):
     sequences = []
-    dedupped = list(set(nums_list))
+    dedupped = sorted(list(set(nums_list)))
     temp_seqs = [[dedupped[0]]]
     for i in range(1, len(dedupped)):
         last_num = dedupped[i-1]
@@ -73,38 +82,108 @@ def find_winning_hands(nums_list):
     winning_hands = []
     completed = []
 
+    if JINGTING_RULES and not(check_terminals_winds_dragons(nums_list)):
+        return winning_hands
+
     pairs = get_pairs(nums_list)
-    possi = find_leftovers(pairs, nums_list)
-    for p in possi:
-        options.append(Hand([p[0]], [], p[1]))
+    combos = find_leftovers(pairs, nums_list)
+    for c in combos:
+        options.append(Hand(c[0], [], [], c[1]))
 
     while len(options) > 0:
         hand = options.pop(0)
-        combos = get_triples(hand.left)
-        combos.extend(get_sequences(hand.left))
-        if len(combos) == 0:
-            continue
+        triple_combos = find_leftovers(get_triples(hand.left), hand.left)
+        sequence_combos = find_leftovers(get_sequences(hand.left), hand.left)
 
-        possi = find_leftovers(combos, hand.left)
-        for p in possi:
-            defined = copy.deepcopy(hand.sets)
-            defined.append(p[0])
-            if len(p[1]) == 0:
-                s_defined = sorted(defined)
-                if not(s_defined in completed):
-                    winning_hands.append(Hand(hand.pair, s_defined, []))
+        for tc in triple_combos:
+            triples = copy.deepcopy(hand.triples)
+            triples.append(tc[0])
+            new_hand = Hand(hand.pair, triples, hand.sequences, tc[1])
+            if len(tc[1]) == 0:
+                if JINGTING_RULES:
+                    if len(new_hand.sequences) == 0:
+                        continue
+                s_defined = sorted(triples + hand.sequences)
+                if not (s_defined in completed):
+                    winning_hands.append(new_hand)
                     completed.append(s_defined)
                 continue
-            options.append(Hand(hand.pair, defined, p[1]))
+            options.append(new_hand)
+
+        for sc in sequence_combos:
+            seqs = copy.deepcopy(hand.sequences)
+            seqs.append(sc[0])
+            new_hand = Hand(hand.pair, hand.triples, seqs, sc[1])
+            if len(sc[1]) == 0:
+                if JINGTING_RULES:
+                    if len(new_hand.triples) == 0:
+                        continue
+                s_defined = sorted(seqs + hand.triples)
+                if not (s_defined in completed):
+                    winning_hands.append(new_hand)
+                    completed.append(s_defined)
+                continue
+            options.append(new_hand)
+
     return winning_hands
 
 
-raw_hand = [2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5]
+# Simple hand 1 [beginning terminal]
+raw_hand = [31, 31, 31, 31, 32, 32, 33, 33, 34, 34, 34]
 winnings = find_winning_hands(raw_hand)
-print(len(winnings))
-pprint.pprint(winnings)
+assert len(winnings) == 2
 
-raw_hand = [3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7]
+# Simple hand 2 [ending terminal]
+raw_hand = [45, 45, 46, 46, 47, 47, 48, 48, 49, 49, 49]
 winnings = find_winning_hands(raw_hand)
-print(len(winnings))
+assert len(winnings) == 2
+
+# Simple hand 3 [wind]
+raw_hand = [11, 11, 32, 32, 33, 33, 34, 34, 35, 35, 35]
+winnings = find_winning_hands(raw_hand)
+assert len(winnings) == 1
+
+# Simple hand 4 [ending dragon]
+raw_hand = [21, 21, 21, 45, 46, 47, 48, 48, 52, 53, 54]
+winnings = find_winning_hands(raw_hand)
+assert len(winnings) == 1
+
+# Hand with no triples
+raw_hand = [51, 51, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+winnings = find_winning_hands(raw_hand)
+assert len(winnings) == 0
+
+# Hand with no sequences
+raw_hand = [11, 11, 11, 13, 13, 21, 21, 21, 23, 23, 23]
+winnings = find_winning_hands(raw_hand)
+assert len(winnings) == 0
+
+# Hand with no terminals or winds or dragons
+raw_hand = [32, 32, 32, 32, 33, 33, 34, 34, 35, 35, 35]
+winnings = find_winning_hands(raw_hand)
+assert len(winnings) == 0
+
+# Full hand
+raw_hand = [17, 17, 25, 25, 25, 32, 33, 34, 45, 45, 45, 57, 58, 59]
+winnings = find_winning_hands(raw_hand)
 pprint.pprint(winnings)
+for w in winnings:
+    pairs = []
+    triples = []
+    sequences = []
+    for p in w.pair:
+        pairs.append(Tile.create_from_comparison_value(p))
+    for t in w.triples:
+        row = []
+        for n in t:
+            row.append(Tile.create_from_comparison_value(n))
+        triples.append(row)
+    for s in w.sequences:
+        row = []
+        for n in s:
+            row.append(Tile.create_from_comparison_value(n))
+        sequences.append(row)
+    print(pairs)
+    print(triples)
+    print(sequences)
+assert len(winnings) == 1
--- tile.py
+++ tile.py
@@ -29,7 +29,7 @@ class Tile:
             str_value = self.value.name
         else:
             str_value = str(self.value)
-        self.name = self.suite.name + " " + str_value
+        self.name = self.suite.name + " - " + str_value
 
     def __repr__(self):
         return self.name
@@ -43,21 +43,43 @@ class Tile:
     def __lt__(self, other):
         return self.comparison_value < other.comparison_value
 
+    @staticmethod
+    def is_terminal_wind_dragon_from_comparison_value(comparison_value):
+        suite = comparison_value // 10
+        value = comparison_value % 10
+        if suite == Tile.Suite.WIND or suite == Tile.Suite.DRAGON:
+            return True
+        if value == 1 or value == 9:
+            return True
+        return False
 
-# w1 = Tile(Tile.Suite.WIND, Tile.Wind.NORTH)
+    @staticmethod
+    def create_from_comparison_value(comparison_value):
+        suite = Tile.Suite(comparison_value // 10)
+        value = comparison_value % 10
+        if suite == Tile.Suite.WIND:
+            tile = Tile(suite, Tile.Wind(value))
+        elif suite == Tile.Suite.DRAGON:
+            tile = Tile(suite, Tile.Dragon(value))
+        else:
+            tile = Tile(suite, value)
+        return tile
+
+
+w1 = Tile(Tile.Suite.WIND, Tile.Wind.NORTH)
 # w2 = Tile(Tile.Suite.WIND, Tile.Wind.EAST)
 # w3 = Tile(Tile.Suite.WIND, Tile.Wind.SOUTH)
 # w4 = Tile(Tile.Suite.WIND, Tile.Wind.WEST)
-# d1 = Tile(Tile.Suite.DRAGON, Tile.Dragon.GREEN)
+d1 = Tile(Tile.Suite.DRAGON, Tile.Dragon.GREEN)
 # d2 = Tile(Tile.Suite.DRAGON, Tile.Dragon.RED)
 # d3 = Tile(Tile.Suite.DRAGON, Tile.Dragon.WHITE)
-# do1 = Tile(Tile.Suite.DOT, 1)
+do1 = Tile(Tile.Suite.DOT, 1)
 # do2 = Tile(Tile.Suite.DOT, 2)
 # do3 = Tile(Tile.Suite.DOT, 3)
 # do4 = Tile(Tile.Suite.DOT, 4)
 # do5 = Tile(Tile.Suite.DOT, 5)
 # st1 = Tile(Tile.Suite.STICK, 1)
-# st2 = Tile(Tile.Suite.STICK, 2)
+st2 = Tile(Tile.Suite.STICK, 2)
 # st3 = Tile(Tile.Suite.STICK, 3)
 # st4 = Tile(Tile.Suite.STICK, 4)
 # st5 = Tile(Tile.Suite.STICK, 5)
@@ -65,6 +87,18 @@ class Tile:
 # nu2 = Tile(Tile.Suite.NUM, 2)
 # nu3 = Tile(Tile.Suite.NUM, 3)
 # nu4 = Tile(Tile.Suite.NUM, 4)
-# nu5 = Tile(Tile.Suite.NUM, 5)
+nu9 = Tile(Tile.Suite.NUM, 9)
 #
-# lt = [w1, w2, w3, w4, d1, d2, d3, do1, do2, do3, do4, do5, st1, st2, st3, st4, st5, nu1, nu2, nu3, nu4, nu5]
+# lt = [w1, w2, w3, w4, d1, d2, d3, do1, do2, do3, do4, do5, st1, st2, st3, st4, st5, nu1, nu2, nu3, nu4, nu9]
+
+assert Tile.is_terminal_wind_dragon_from_comparison_value(w1.comparison_value)
+assert Tile.is_terminal_wind_dragon_from_comparison_value(d1.comparison_value)
+assert Tile.is_terminal_wind_dragon_from_comparison_value(do1.comparison_value)
+assert not(Tile.is_terminal_wind_dragon_from_comparison_value(st2.comparison_value))
+assert Tile.is_terminal_wind_dragon_from_comparison_value(nu9.comparison_value)
+
+assert Tile.create_from_comparison_value(w1.comparison_value) == w1
+assert Tile.create_from_comparison_value(d1.comparison_value) == d1
+assert Tile.create_from_comparison_value(do1.comparison_value) == do1
+assert Tile.create_from_comparison_value(st2.comparison_value) == st2
+assert Tile.create_from_comparison_value(nu9.comparison_value) == nu9
