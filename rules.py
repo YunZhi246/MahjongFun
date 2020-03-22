0a47ca6315ee28ff65ee7be0b593c895d84b3e28
Yun Zhi Lin <yunzhi246@gmail.com>
2020-03-21 21:22:32 -0400

    rule description

--- rules.py
+++ rules.py
@@ -1,8 +1,18 @@
 from enum import Enum
 
+# triple = three tiles of the exact same value
+# sequence = three consecutive dot, stick or number tiles
+
+# Rules:
+# Basic - a complete hand consists of one pair and the rest of the tiles form triples and sequences
+# Jingting - includes basic rules and a complete hand must contain the following:
+#   1. a terminal, wind or dragon
+#   2. at least one triple
+#   3. at least one sequence
+
 
 class Rules(Enum):
-    Normal = 1
+    Basic = 1
     Jingting = 2
 
 
